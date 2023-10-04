237097c53cc22c78c537421894d4649839972570
Paul Duncan <pabs@pablotron.org>
2023-10-04 15:19:20 -0400

    fips203.h: add KEM768 functions

--- fips203.h
+++ fips203.h
@@ -11,4 +11,12 @@ void fips203_kem512_keygen(uint8_t ek[static FIPS203_KEM512_EK_SIZE], uint8_t dk
 void fips203_kem512_encaps(uint8_t k[static 32], uint8_t ct[static FIPS203_KEM512_CT_SIZE], const uint8_t ek[static FIPS203_KEM512_EK_SIZE], const uint8_t seed[static 32]);
 void fips203_kem512_decaps(uint8_t k[static 32], const uint8_t ct[static FIPS203_KEM512_CT_SIZE], const uint8_t dk_kem[static FIPS203_KEM512_DK_SIZE]);
 
+#define FIPS203_KEM768_EK_SIZE 1184 /* 384 * K + 32 */
+#define FIPS203_KEM768_DK_SIZE 2400 /* 768 * K + 96 */
+#define FIPS203_KEM768_CT_SIZE 1088 /* 32 * (DU * K + DV) */
+
+void fips203_kem768_keygen(uint8_t ek[static FIPS203_KEM768_EK_SIZE], uint8_t dk[static FIPS203_KEM768_DK_SIZE], const uint8_t seed[static 64]);
+void fips203_kem768_encaps(uint8_t k[static 32], uint8_t ct[static FIPS203_KEM768_CT_SIZE], const uint8_t ek[static FIPS203_KEM768_EK_SIZE], const uint8_t seed[static 32]);
+void fips203_kem768_decaps(uint8_t k[static 32], const uint8_t ct[static FIPS203_KEM768_CT_SIZE], const uint8_t dk_kem[static FIPS203_KEM768_DK_SIZE]);
+
 #endif /* FIPS203_H */

30200d5fcaee8e420c25c2d3c69038c2e49cdb86
Paul Duncan <pabs@pablotron.org>
2023-10-04 13:08:35 -0400

    tests/codec.rb: clean up generated encode tables

--- tests/codec.rb
+++ tests/codec.rb
@@ -2,19 +2,25 @@
 # frozen_string_literal: true
 
 #
-# generate encode/decode lookup tables for various bit widths
+# generate encode/decode lookup tables for various bit sizes.
+#
+# - KEM512 use 1, 4, and 10
+# - KEM768 use 1, 4, and 10
+# - KEM1024 use 1, 5, and 11
+#
+# The output of this script is embedded in the top-level file
+# `fips203.c`.
 #
 
-DS = [1, 4, 10] # bit lengths
+DS = [4, 10] # bit lengths (1 excluded)
 Q = 3329 # modulus
 
 # templates
 T = {
-  encode_lut: %{static const struct { uint16_t lo, hi; } ENCODE%<d>d_LUT[] = {\n%<rows>s\n};},
+  encode_lut: %{// Polynomial coefficient %<d>d-bit encoding lookup table\n// (used by `poly_encode_%<d>dbit()`)\nstatic const struct { uint16_t lo, hi; } ENCODE%<d>d_LUT[] = {\n%<rows>s\n};},
   encode_row: %{  { %<lo>d, %<hi>d }, // %<i>d},
-  decode_lut: %{static const uint16_t DECODE%<d>d_LUT[] = {\n%<rows>s\n};},
+  decode_lut: %{// polynomial coefficient %<d>d-bit decoding lookup table\n// (used by `poly_decode_%<d>dbit()`)\nstatic const uint16_t DECODE%<d>d_LUT[] = {\n%<rows>s\n};},
   decode_row: %{  %<y>d, // %<x>d},
-  tests: %{// poly_encode_%<d>dbits() tests (0-3315, step=13)\n.name = "0-3315 (inc by 13)",\n.val = { .cs = { %<src>s } }\n.exp = { .cs = { %<dst>s } },},
 }
 
 # x -> round(2^d/Q * x)
@@ -27,12 +33,12 @@ def decode(x, d)
   Rational(Q * x, 2**d).round
 end
 
-# get encode LUT for given bit length
+# get encode LUT for given bit length `d`
 def encode_lut(d)
   lut = Q.times.each_with_object(Hash.new { |h, k| h[k] = [3329, 0] }) do |x, lut|
     y = encode(x, d)
-    lut[y][0] = x if x < lut[y][0]
-    lut[y][1] = x if x > lut[y][1]
+    lut[y][0] = x if x < lut[y][0] # set new lower bound
+    lut[y][1] = x if x > lut[y][1] # set new upper bound
   end
 
   T[:encode_lut] % {
@@ -43,7 +49,7 @@ def encode_lut(d)
   }
 end
 
-# get decode LUT for given bit length
+# get decode LUT for given bit length `d`
 def decode_lut(d)
   T[:decode_lut] % {
     d: d,
@@ -52,15 +58,8 @@ def decode_lut(d)
     }.join("\n"),
   }
 end
-  
-# print LUTs for all bit lengths
-puts(DS.map { |d|
-  [encode_lut(d), decode_lut(d)].join("\n\n")
-}.join("\n\n"))
 
-# print encode tests
-puts(DS.map { |d|
-  src = (0...Q).step(13).to_a
-  dst = src.map { |x| decode(encode(x, d), d) % Q }
-  T[:tests] % { d: d, src: src.join(', '), dst: dst.join(', ') }
-}.join("\n\n"))
+# print tables to stdout
+puts DS.map { |d| encode_lut(d) }.join("\n\n"),
+     '', # blank line to delimit encode/decode tables
+     DS.map { |d| decode_lut(d) }.join("\n\n")
